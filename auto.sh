4b46e4affe7203eada8947adacc9a378a0f62e26
Fei Zhan <enfeizhan@gmail.com>
2016-07-10 14:44:51 +1000

    auto updated to new interface

--- auto.sh
+++ auto.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-cd /home/fei/Documents/studies/Project2M
-source /home/fei/miniconda3/bin/activate Project2M 
+cd ~/Documents/studies/Project2M
+source ~/miniconda3/bin/activate project2m 
 # run routine file
-python /home/fei/Documents/studies/Project2M/daily_updating.py
-source /home/fei/miniconda3/bin/activate python2
-# send notification emails
-cd /home/fei/Documents/studies/Project2M/send_email
-python /home/fei/Documents/studies/Project2M/send_email/gmail_api.py
-source /home/fei/miniconda3/bin/deactivate
+python bulk_update.py sector auto
+python bulk_update.py share auto
+source ~/miniconda3/bin/deactivate
+# source /home/fei/miniconda3/bin/activate python2
+# # send notification emails
+# cd /home/fei/Documents/studies/Project2M/send_email
+# python /home/fei/Documents/studies/Project2M/send_email/gmail_api.py
+# source /home/fei/miniconda3/bin/deactivate
