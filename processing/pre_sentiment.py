55118415895a5273879a6867a251c82f59651051
Fei Zhan <enfeizhan@gmail.com>
2016-08-06 22:04:17 +1000

    codes peped

--- processing/pre_sentiment.py
+++ processing/pre_sentiment.py
@@ -11,28 +11,21 @@ logging.basicConfig(
     level=logging.INFO
 )
 db_url = os.path.expanduser('~/Dropbox/Project2M/Sentiment')
-# cmd_doc = '''
-#     Usage:
-#       bulk_update share auto [--share-back-days=days] [--business=b] [--share-url=url] [--source=source] [--codes=codes]
-#       bulk_update share manual <start> <end> [--business=b] [--share-url=url] [--source=source] [--codes=codes]
-#       bulk_update sector auto [--sector-back-days=days] [--business=b] [--sector-url=url] [--source=source] [--codes=codes]
-#       bulk_update sector manual <start> <end> [--business=b] [--sector-url=url] [--source=source] [--codes=codes]
-# 
-#     Options:
-#       -h --help     Show this screen.
-#       -c --codes=codes  ASX codes separated by comma. Mainly for debugging and testing purposes.
-#       --share-back-days=days  Days to look backward for shares [default: 0].
-#       --sector-back-days=days  Days to look backward for sectors [default: 1].
-#       --business=b  Only look at business days [default: True].
-#       --share-url=url  URL to find share file [default: ~/Dropbox/Project2M/ASXYearlyCompanyConsolidation/].
-#       --sector-url=url  URL to find sector file [default: ~/Dropbox/Project2M/ASXYearlySectorConsolidation/].
-#       --source=source  Data source [default: yahoo].
-# '''
 today = pd.datetime.today()
 year = today.year
 month = today.month
 day = today.day
 today = '{:4d}-{:02d}-{:02d}'.format(year, month, day)
+
+
+def save_counts(code_list, source, date):
+    code_series = pd.Series(code_list)
+    code_counts = code_series.value_counts()
+    code_counts = code_counts.reset_index()
+    code_counts.loc[:, 'source'] = source
+    code_counts.loc[:, 'date'] = date
+    code_counts.columns = ['asx code', 'counts', 'source', 'date']
+    return code_counts
 
 
 def scrape_motley_fool():
@@ -45,16 +38,33 @@ def scrape_motley_fool():
     link_href_list = []
     all_on_today = True
     while all_on_today:
-        article_list = np.asarray(driver.find_elements_by_class_name('article-list'))
-        auth_date_list = [article.find_element_by_tag_name('h6').text for article in article_list]
-        date_list = pd.to_datetime([re.search(date_pattern, date).groups()[0] for date in auth_date_list])
+        # collect all article items
+        article_list = np.asarray(
+            driver.find_elements_by_class_name('article-list')
+        )
+        # collect dates from below article title
+        auth_date_list = [
+            article.find_element_by_tag_name('h6').text
+            for article in article_list
+        ]
+        date_list = pd.to_datetime(
+            [re.search(date_pattern, date).groups()[0]
+             for date in auth_date_list]
+        )
+        # check if article is from today
         is_today= date_list == today
         article_list = article_list[is_today]
-        link_href_list += [article.find_element_by_tag_name('a').get_attribute('href') for article in article_list]
+        # get today's articles link
+        link_href_list += [
+            article.find_element_by_tag_name('a').get_attribute('href')
+            for article in article_list
+        ]
+        # if all articles are for today, click to next page to check if more
+        # today's article on the next page
         all_on_today = is_today.all()
         if all_on_today:
-            next_button = driver.find_element_by_css_selector('a.next.pagination')
-            next_button.click()
+            # find the "next page" button and click
+            driver.find_element_by_css_selector('a.next.pagination').click()
     code_list = []
     index_list = []
     for link in link_href_list:
@@ -98,39 +108,6 @@ def scrape_hotcopper_forum():
     return res_df
 
 
-def save_counts(
-        code_list,
-        source,
-        date,
-        ):
-    code_series = pd.Series(code_list)
-    code_counts = code_series.value_counts()
-    code_counts = code_counts.reset_index()
-    code_counts.loc[:, 'source'] = source
-    code_counts.loc[:, 'date'] = date
-    code_counts.columns = ['asx code', 'counts', 'source', 'date']
-    return code_counts
-
-
-if __name__ == '__main__':
-    # arguments = docopt(cmd_doc)
-    code_counts, index_counts = scrape_motley_fool()
-    code_counts.to_csv(
-        os.path.join(db_url, 'code_counts.csv'),
-        index=False,
-        mode='a',
-        header=None
-    )
-    index_counts.to_csv(
-        os.path.join(db_url, 'index_counts.csv'),
-        index=False,
-        mode='a',
-        header=None
-    )
-    code_counts = scrape_hotcopper_forum()
-    code_counts.to_csv(
-        os.path.join(db_url, 'code_counts.csv'),
-        index=False,
-        mode='a',
-        header=None
-    )
+def run_pre_sentiment():
+    scrape_motley_fool()
+    scrape_hotcopper_forum()
